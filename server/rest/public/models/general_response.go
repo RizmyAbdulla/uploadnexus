54ab7fce014af7e68ce0898de66757367f11141e
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-07-31 18:37:10 +0530

    created general response object to keep responses unified

--- server/rest/public/models/general_response.go
+++ server/rest/public/models/general_response.go
@@ -1,7 +1,15 @@
 package models
 
-type Response struct {
+type GeneralResponse struct {
 	Code    int         `json:"code"`
 	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
+	Data    interface{} `json:"data,omitempty"`
+}
+
+func NewGeneralResponse(code int, message string, data interface{}) *GeneralResponse {
+	return &GeneralResponse{
+		Code:    code,
+		Message: message,
+		Data:    data,
+	}
 }
