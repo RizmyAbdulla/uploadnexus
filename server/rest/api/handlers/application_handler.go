3cfbb52b6830def96a35635873be2b6a3f347e30
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-07-31 18:32:39 +0530

    create handler to create application

--- server/rest/api/handlers/application_handler.go
+++ server/rest/api/handlers/application_handler.go
@@ -1,1 +1,46 @@
 package handlers
+
+import (
+	"github.com/ArkamFahry/uploadnexus/server/rest/api/services"
+	"github.com/ArkamFahry/uploadnexus/server/rest/public/models"
+	"github.com/ArkamFahry/uploadnexus/server/rest/storage/database"
+	"github.com/ArkamFahry/uploadnexus/server/rest/utils"
+	"github.com/gofiber/fiber/v2"
+)
+
+func RegisterApplicationRoutes(api fiber.Router) {
+	databaseClient := database.GetDatabaseClient()
+	applicationService := services.NewApplicationService(databaseClient)
+	applicationHandler := NewApplicationHandler(applicationService)
+
+	applicationRoute := api.Group("/applications")
+	applicationRoute.Post("/", applicationHandler.CreateApplication)
+}
+
+type IApplicationHandler interface {
+	CreateApplication(ctx *fiber.Ctx) error
+}
+
+type ApplicationHandler struct {
+	applicationService services.IApplicationService
+}
+
+var _ IApplicationHandler = (*ApplicationHandler)(nil)
+
+func NewApplicationHandler(applicationService services.IApplicationService) *ApplicationHandler {
+	return &ApplicationHandler{
+		applicationService: applicationService,
+	}
+}
+
+func (h *ApplicationHandler) CreateApplication(ctx *fiber.Ctx) error {
+	var request models.Application
+	utils.ParseRequestBody(ctx, &request)
+
+	response, err := h.applicationService.CreateApplication(ctx.Context(), request)
+	if err != nil {
+		return ctx.Status(err.Code).JSON(err)
+	}
+
+	return ctx.Status(response.Code).JSON(response)
+}
