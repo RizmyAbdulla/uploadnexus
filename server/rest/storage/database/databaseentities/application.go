9f28218d86519655f1057538350af00ee09e5a4e
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-07-31 18:39:02 +0530

    added db tags to Application for use with the db

--- server/rest/storage/database/databaseentities/application.go
+++ server/rest/storage/database/databaseentities/application.go
@@ -1,11 +1,11 @@
 package databaseentities
 
 type Application struct {
-	Id          string  `json:"id"`
-	Name        string  `json:"name"`
-	Description *string `json:"description"`
-	CreatedAt   int64   `json:"created_at"`
-	UpdatedAt   *int64  `json:"updated_at"`
+	Id          string  `json:"id" db:"id"`
+	Name        string  `json:"name" db:"name"`
+	Description *string `json:"description" db:"description"`
+	CreatedAt   int64   `json:"created_at" db:"created_at"`
+	UpdatedAt   *int64  `json:"updated_at" db:"updated_at"`
 }
 
 func (Application) CollectionName() string {
