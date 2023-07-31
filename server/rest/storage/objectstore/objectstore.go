71970d6d79e987cadd520455580f6bf938859169
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-07-31 12:12:21 +0530

    added object store init

--- server/rest/storage/objectstore/objectstore.go
+++ server/rest/storage/objectstore/objectstore.go
@@ -1,1 +1,30 @@
 package objectstore
+
+import (
+	"github.com/ArkamFahry/uploadnexus/server/rest/constants"
+	"github.com/ArkamFahry/uploadnexus/server/rest/envs"
+	"github.com/ArkamFahry/uploadnexus/server/rest/errors"
+	"github.com/ArkamFahry/uploadnexus/server/rest/storage/objectstore/objectstoreclients"
+	"github.com/ArkamFahry/uploadnexus/server/rest/storage/objectstore/objectstoreclients/minio"
+)
+
+var Client objectstoreclients.ObjectStoreClient
+
+func InitObjectStore() error {
+	const Op errors.Op = "objectstore.InitObjectStore"
+	var err error
+	isMinio := envs.EnvStoreInstance.GetEnv().ObjectStoreType == constants.ObjectStoreTypeMinio
+
+	if isMinio {
+		Client, err = minio.NewClient()
+		if err != nil {
+			return errors.NewError(Op, errors.Msg("failed to create object store provider"), err)
+		}
+	}
+
+	return nil
+}
+
+func GetObjectStoreClient() objectstoreclients.ObjectStoreClient {
+	return Client
+}
