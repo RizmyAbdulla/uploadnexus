972734fb79d797a3c2897ee120526c90497b3645
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-07-31 12:11:55 +0530

    added db inti

--- server/rest/storage/database/database.go
+++ server/rest/storage/database/database.go
@@ -1,1 +1,31 @@
 package database
+
+import (
+	"github.com/ArkamFahry/uploadnexus/server/rest/constants"
+	"github.com/ArkamFahry/uploadnexus/server/rest/envs"
+	"github.com/ArkamFahry/uploadnexus/server/rest/errors"
+	"github.com/ArkamFahry/uploadnexus/server/rest/storage/database/databaseclients"
+	"github.com/ArkamFahry/uploadnexus/server/rest/storage/database/databaseclients/postgresql"
+)
+
+var Client databaseclients.DatabaseClient
+
+func InitDatabase() error {
+	const Op errors.Op = "database.InitDatabase"
+	var err error
+
+	isPostgreSQL := envs.EnvStoreInstance.GetEnv().DatabaseType == constants.DatabaseTypePostgreSQL
+
+	if isPostgreSQL {
+		Client, err = postgresql.NewClient()
+		if err != nil {
+			return errors.NewError(Op, errors.Msg("failed to create database provider"), err)
+		}
+	}
+
+	return nil
+}
+
+func GetDatabaseClient() databaseclients.DatabaseClient {
+	return Client
+}
