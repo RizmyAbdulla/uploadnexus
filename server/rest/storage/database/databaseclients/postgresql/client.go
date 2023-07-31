334f10195e35fcd826de16be78bca3a2cbef6fcc
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-07-31 12:14:05 +0530

    setup the postgresql client

---
+++ server/rest/storage/database/databaseclients/postgresql/client.go
@@ -0,0 +1,45 @@
+package postgresql
+
+import (
+	"fmt"
+	"github.com/ArkamFahry/uploadnexus/server/rest/envs"
+	"github.com/ArkamFahry/uploadnexus/server/rest/errors"
+	"github.com/ArkamFahry/uploadnexus/server/rest/storage/database/migrations"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+type DatabaseClient struct {
+	db *sqlx.DB
+}
+
+func NewClient() (*DatabaseClient, error) {
+	const Op errors.Op = "postgresql.NewClient"
+	var err error
+	var db *sqlx.DB
+
+	postgresqlHost := envs.EnvStoreInstance.GetEnv().DatabaseHost
+	postgresqlPort := envs.EnvStoreInstance.GetEnv().DatabasePort
+	postgresqlUser := envs.EnvStoreInstance.GetEnv().DatabaseUser
+	postgresqlPassword := envs.EnvStoreInstance.GetEnv().DatabasePassword
+	postgresqlDatabaseName := envs.EnvStoreInstance.GetEnv().DatabaseName
+	postgresqlSsl := envs.EnvStoreInstance.GetEnv().DatabaseSsl
+
+	postgresqlUrl := fmt.Sprintf("postgresql://%s:%s@%s:%s/%s?sslmode=%s", postgresqlUser, postgresqlPassword, postgresqlHost, postgresqlPort, postgresqlDatabaseName, postgresqlSsl)
+
+	err = migrations.MigrateDatabase(migrations.PostgresqlMigrations, migrations.PostgresqlMigrationsFolder, postgresqlUrl)
+	if err != nil {
+		return nil, errors.NewError(Op, errors.Msg("failed to migrate database"), err)
+	}
+
+	db, err = sqlx.Open("postgres", postgresqlUrl)
+	if err != nil {
+		return nil, errors.NewError(Op, errors.Msg("failed to open database connection"), err)
+	}
+
+	return &DatabaseClient{
+		db: db,
+	}, nil
+}
