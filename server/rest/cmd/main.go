7835c5fe486f970a066de323ceb8efdda1faf323
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-08-01 13:52:00 +0530

    added recover middleware

--- server/rest/cmd/main.go
+++ server/rest/cmd/main.go
@@ -4,35 +4,40 @@ import (
 	"github.com/ArkamFahry/uploadnexus/server/rest/api/handlers"
 	"github.com/ArkamFahry/uploadnexus/server/rest/config"
 	"github.com/ArkamFahry/uploadnexus/server/rest/envs"
-	"github.com/ArkamFahry/uploadnexus/server/rest/errors"
+	"github.com/ArkamFahry/uploadnexus/server/rest/exceptions"
 	"github.com/ArkamFahry/uploadnexus/server/rest/storage/database"
 	"github.com/ArkamFahry/uploadnexus/server/rest/storage/objectstore"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/rs/zerolog/log"
 )
 
 func main() {
-	const Op errors.Op = "app.Serve"
+	const Op exceptions.Op = "app.Serve"
 	var err error
 
 	config.InitZeroLogger()
 
 	err = envs.InitEnv()
 	if err != nil {
-		log.Fatal().Msg(errors.NewError(Op, errors.Msg("error initializing env"), err).Error())
+		log.Fatal().Msg(exceptions.NewError(Op, "error initializing env", err).Error())
 	}
 
 	err = database.InitDatabase()
 	if err != nil {
-		log.Fatal().Msg(errors.NewError(Op, errors.Msg("error initializing database"), err).Error())
+		log.Fatal().Msg(exceptions.NewError(Op, "error initializing database", err).Error())
 	}
 
 	err = objectstore.InitObjectStore()
 	if err != nil {
-		log.Fatal().Msg(errors.NewError(Op, errors.Msg("error initializing object store"), err).Error())
+		log.Fatal().Msg(exceptions.NewError(Op, "error initializing object store", err).Error())
 	}
 
 	app := fiber.New()
+
+	recoverMiddleware := recover.New()
+
+	app.Use(recoverMiddleware)
 
 	api := app.Group("/api")
 	handlers.RegisterHealthRoutes(api)
@@ -42,6 +47,6 @@ func main() {
 
 	err = app.Listen(":" + appPort)
 	if err != nil {
-		log.Fatal().Msg(errors.NewError(Op, errors.Msg("error while starting server"), err).Error())
+		log.Fatal().Msg(exceptions.NewError(Op, "error while starting server", err).Error())
 	}
 }
