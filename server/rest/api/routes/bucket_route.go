519d4450c98aebe4ad0c8a947c17015aaf489444
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-08-04 21:59:42 +0530

    implemented the base bucket routes

---
+++ server/rest/api/routes/bucket_route.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"github.com/ArkamFahry/uploadnexus/server/rest/api/handlers"
+	"github.com/ArkamFahry/uploadnexus/server/rest/api/services"
+	"github.com/ArkamFahry/uploadnexus/server/rest/storage/database"
+	"github.com/gofiber/fiber/v2"
+)
+
+func RegisterBucketRoutes(api fiber.Router) {
+	databaseClient := database.GetDatabaseClient()
+	bucketService := services.NewBucketService(databaseClient)
+	bucketHandler := handlers.NewBucketHandler(bucketService)
+
+	bucketRouter := api.Group("/buckets")
+	bucketRouter.Post("/", bucketHandler.CreateBucket)
+	bucketRouter.Patch("/:id", bucketHandler.UpdateBucket)
+	bucketRouter.Get("/:id", bucketHandler.GetBucketById)
+}
