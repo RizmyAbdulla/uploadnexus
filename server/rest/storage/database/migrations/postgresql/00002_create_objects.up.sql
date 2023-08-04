a883f296c20d4e85bbe1dec9d9efa6729672fd48
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-08-04 21:58:48 +0530

    made object more simple

--- server/rest/storage/database/migrations/postgresql/00002_create_objects.up.sql
+++ server/rest/storage/database/migrations/postgresql/00002_create_objects.up.sql
@@ -1,14 +1,13 @@
 create table if not exists objects
 (
-    id               char(36) primary key,
-    bucket_reference char(36)     not null,
-    file_key         text         not null,
-    file_name        text         not null,
-    file_type        varchar(255) not null,
-    file_size        bigint       not null,
-    upload_status    varchar(255) not null,
-    metadata         jsonb,
-    created_at       bigint       not null,
-    updated_at       bigint,
-    unique (bucket_reference, file_key)
+    id            char(36) primary key,
+    bucket        char(36)     not null,
+    name          text         not null,
+    mime_type     varchar(255) not null,
+    size          bigint       not null,
+    upload_status varchar(255) not null,
+    metadata      text,
+    created_at    bigint       not null,
+    updated_at    bigint,
+    unique (bucket, name)
 );
