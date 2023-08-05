83d2bc87553c8de4e93da0d9b8f7a6eb32a0893f
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-08-05 19:55:05 +0530

    renamed from Url to Object to make it more concise

--- server/storage/objectstore/objectstore.go
+++ server/storage/objectstore/objectstore.go
@@ -11,8 +11,8 @@ import (
 )
 
 type StoreClient interface {
-	CreatePresignedPutUrl(ctx context.Context, bucketName string, objectName string, expiry int64) (string, error)
-	CratedPresignedGetUrl(ctx context.Context, bucketName string, objectName string, expiry int64) (string, error)
+	CreatePresignedPutObject(ctx context.Context, bucketName string, objectName string, expiry int64) (string, error)
+	CratedPresignedGetObject(ctx context.Context, bucketName string, objectName string, expiry int64) (string, error)
 
 	DeleteObject(ctx context.Context, bucketName string, objectName string) error
 }
