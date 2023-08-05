dd63d570c2e8bb64fd2f6ba4e5fa8dd1989f83ea
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-08-05 13:04:15 +0530

    simplified objectstore to only use S3 client

---
+++ server/rest/storage/objectstore/object.go
@@ -0,0 +1,22 @@
+package objectstore
+
+import (
+	"context"
+	"github.com/ArkamFahry/uploadnexus/server/rest/errors"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+func (c *S3Client) DeleteObject(ctx context.Context, bucketName string, objectName string) error {
+	const Op errors.Op = "objectstore.DeleteObject"
+
+	_, err := c.s3Client.DeleteObject(ctx, &s3.DeleteObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectName),
+	})
+	if err != nil {
+		return errors.NewError(Op, errors.Msg("failed to delete object"), err)
+	}
+
+	return nil
+}
---
+++ server/rest/storage/objectstore/pre_signed_url.go
@@ -0,0 +1,40 @@
+package objectstore
+
+import (
+	"context"
+	"github.com/ArkamFahry/uploadnexus/server/rest/errors"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"time"
+)
+
+func (c *S3Client) CreatePresignedPutUrl(ctx context.Context, bucketName string, objectName string, expiry int64) (string, error) {
+	const Op errors.Op = "objectstore.CreatePresignedPutUrl"
+
+	expiryTime := time.Unix(expiry, 0)
+
+	url, err := c.s3PresignedClient.PresignPutObject(ctx, &s3.PutObjectInput{
+		Bucket:  aws.String(bucketName),
+		Key:     aws.String(objectName),
+		Expires: &expiryTime,
+	})
+	if err != nil {
+		return "", errors.NewError(Op, "failed to create presigned put url", err)
+	}
+
+	return url.URL, nil
+}
+
+func (c *S3Client) CratedPresignedGetUrl(ctx context.Context, bucketName string, objectName string, expiry int64) (string, error) {
+	const Op errors.Op = "objectstore.CreatePresignedPutUrl"
+
+	url, err := c.s3PresignedClient.PresignGetObject(ctx, &s3.GetObjectInput{
+		Bucket: aws.String(bucketName),
+		Key:    aws.String(objectName),
+	})
+	if err != nil {
+		return "", errors.NewError(Op, "failed to create presigned get url", err)
+	}
+
+	return url.URL, nil
+}
