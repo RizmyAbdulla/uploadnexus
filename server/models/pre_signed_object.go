114345a4c600a89b23866ec643e479bbecac3b4a
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-08-06 13:15:49 +0530

    renamed PreSignedUrl to PreSignedObject

--- server/models/pre_signed_object.go
+++ server/models/pre_signed_object.go
@@ -1,14 +1,9 @@
 package models
 
-type PreSignedUrl struct {
+type PreSignedObject struct {
 	BucketName string `json:"bucket_name"`
 	ObjectName string `json:"object_name"`
 	Url        string `json:"url"`
 	HttpMethod string `json:"http_method"`
 	Expiry     int64  `json:"expiry"`
-}
-
-type PreSignedUrlCreate struct {
-	BucketName string `json:"bucket_name" validate:"required"`
-	ObjectName string `json:"object_name" validate:"required"`
 }
