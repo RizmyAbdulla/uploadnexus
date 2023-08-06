116ebaef438badec6fb4ba38e5efac26be62a2c5
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-08-06 21:14:28 +0530

    made the validator into injectable service

--- server/api/routes/bucket_route.go
+++ server/api/routes/bucket_route.go
@@ -4,12 +4,14 @@ import (
 	"github.com/ArkamFahry/uploadnexus/server/api/handlers"
 	"github.com/ArkamFahry/uploadnexus/server/api/services"
 	"github.com/ArkamFahry/uploadnexus/server/storage/database"
+	"github.com/ArkamFahry/uploadnexus/server/utils"
 	"github.com/gofiber/fiber/v2"
 )
 
 func RegisterBucketRoutes(api fiber.Router) {
 	databaseClient := database.GetDatabaseClient()
-	bucketService := services.NewBucketService(databaseClient)
+	modelValidator := utils.NewModelValidator()
+	bucketService := services.NewBucketService(databaseClient, modelValidator)
 	bucketHandler := handlers.NewBucketHandler(bucketService)
 
 	bucketRouter := api.Group("/bucket")
