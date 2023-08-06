a2a55f3cb29aa8d1816c7d3be1bfc44c3ba13a01
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-08-06 13:11:31 +0530

    implemented object handler

--- server/api/handlers/object_handler.go
+++ server/api/handlers/object_handler.go
@@ -1,10 +1,62 @@
 package handlers
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/ArkamFahry/uploadnexus/server/api/services"
+	"github.com/ArkamFahry/uploadnexus/server/utils"
+	"github.com/gofiber/fiber/v2"
+)
 
 type IObjectHandler interface {
-	GetObject(ctx *fiber.Ctx) error
+	CreatePresignedPutObject(ctx *fiber.Ctx) error
+	CreatePresignedGetObject(ctx *fiber.Ctx) error
 }
 
 type ObjectHandler struct {
+	objectService services.IObjectService
+}
+
+var _ IObjectHandler = (*ObjectHandler)(nil)
+
+func NewObjectHandler(objectService services.IObjectService) *ObjectHandler {
+	return &ObjectHandler{
+		objectService: objectService,
+	}
+}
+
+func (h *ObjectHandler) CreatePresignedPutObject(ctx *fiber.Ctx) error {
+	bucketName, err := utils.GetParamBucketName(ctx)
+	if err != nil {
+		return ctx.Status(err.Code).JSON(err)
+	}
+
+	wildCard, err := utils.GetParamWildcard(ctx)
+	if err != nil {
+		return ctx.Status(err.Code).JSON(err)
+	}
+
+	response, err := h.objectService.CreatePreSignedPutObject(ctx.Context(), bucketName, wildCard)
+	if err != nil {
+		return ctx.Status(err.Code).JSON(err)
+	}
+
+	return ctx.Status(response.Code).JSON(response)
+}
+
+func (h *ObjectHandler) CreatePresignedGetObject(ctx *fiber.Ctx) error {
+	bucketName, err := utils.GetParamBucketName(ctx)
+	if err != nil {
+		return ctx.Status(err.Code).JSON(err)
+	}
+
+	wildCard, err := utils.GetParamWildcard(ctx)
+	if err != nil {
+		return ctx.Status(err.Code).JSON(err)
+	}
+
+	response, err := h.objectService.CreatePreSignedGetObject(ctx.Context(), bucketName, wildCard)
+	if err != nil {
+		return ctx.Status(err.Code).JSON(err)
+	}
+
+	return ctx.Status(response.Code).JSON(response)
 }
