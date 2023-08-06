e79910b9a7ee59ba2c03e555f4c473d591a74685
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-08-06 09:48:55 +0530

    changed file_size_limit to allowed_object_size

--- server/storage/database/migrations/postgresql/00001_create_buckets.up.sql
+++ server/storage/database/migrations/postgresql/00001_create_buckets.up.sql
@@ -1,12 +1,12 @@
 create table if not exists buckets
 (
-    id                 char(36) primary key,
-    name               varchar(255) not null,
-    description        text,
-    allowed_mime_types text[]       not null,
-    file_size_limit    bigint       not null,
-    is_public          boolean      not null,
-    created_at         bigint       not null,
-    updated_at         bigint,
+    id                  char(36) primary key,
+    name                varchar(255) not null,
+    description         text,
+    allowed_mime_types  text[]       not null,
+    allowed_object_size bigint       not null,
+    is_public           boolean      not null,
+    created_at          bigint       not null,
+    updated_at          bigint,
     unique (name)
 );
