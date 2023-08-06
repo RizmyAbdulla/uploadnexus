f3bd604ce1b615a4721ce23ee7a8278ff5ab552e
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-08-06 13:16:14 +0530

    added logical bucket portioning

--- server/storage/objectstore/pre_signed_object.go
+++ server/storage/objectstore/pre_signed_object.go
@@ -2,6 +2,8 @@ package objectstore
 
 import (
 	"context"
+	"fmt"
+	"github.com/ArkamFahry/uploadnexus/server/envs"
 	"github.com/ArkamFahry/uploadnexus/server/errors"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -11,11 +13,13 @@ import (
 func (c *S3Client) CreatePresignedPutObject(ctx context.Context, bucketName string, objectName string, expiry int64) (string, error) {
 	const Op errors.Op = "objectstore.CreatePresignedPutObject"
 
+	bucket := envs.EnvStoreInstance.GetEnv().BucketName
 	expiryDuration := time.Duration(expiry) * time.Second
+	object := fmt.Sprintf("%s/%s", bucketName, objectName)
 
 	url, err := c.s3PresignedClient.PresignPutObject(ctx, &s3.PutObjectInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(objectName),
+		Bucket: aws.String(bucket),
+		Key:    aws.String(object),
 	}, s3.WithPresignExpires(expiryDuration))
 	if err != nil {
 		return "", errors.NewError(Op, "failed to create presigned put url", err)
@@ -24,14 +28,16 @@ func (c *S3Client) CreatePresignedPutObject(ctx context.Context, bucketName stri
 	return url.URL, nil
 }
 
-func (c *S3Client) CratedPresignedGetObject(ctx context.Context, bucketName string, objectName string, expiry int64) (string, error) {
+func (c *S3Client) CreatePresignedGetObject(ctx context.Context, bucketName string, objectName string, expiry int64) (string, error) {
 	const Op errors.Op = "objectstore.CreatePresignedPutObject"
 
+	bucket := envs.EnvStoreInstance.GetEnv().BucketName
 	expiryDuration := time.Duration(expiry) * time.Second
+	object := fmt.Sprintf("%s/%s", bucketName, objectName)
 
 	url, err := c.s3PresignedClient.PresignGetObject(ctx, &s3.GetObjectInput{
-		Bucket: aws.String(bucketName),
-		Key:    aws.String(objectName),
+		Bucket: aws.String(bucket),
+		Key:    aws.String(object),
 	}, s3.WithPresignExpires(expiryDuration))
 	if err != nil {
 		return "", errors.NewError(Op, "failed to create presigned get url", err)
