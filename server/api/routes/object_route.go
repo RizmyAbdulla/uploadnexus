d5e187575618fc3aabc32a0d6fb02a5080fc6576
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-08-06 10:00:18 +0530

    created the object router

--- server/api/routes/object_route.go
+++ server/api/routes/object_route.go
@@ -1,1 +1,10 @@
 package routes
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func RegisterObjectRoutes(api fiber.Router) {
+	objectRoute := api.Group("/object")
+	objectRoute.Get("/sign", nil)
+}
