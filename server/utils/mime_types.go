29e35e0c6925e957391cc1b012df49c8faccf623
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-08-06 17:41:46 +0530

    added mime type getter and a validator

--- server/utils/mime_types.go
+++ server/utils/mime_types.go
@@ -2,8 +2,20 @@ package utils
 
 import (
 	"fmt"
+	"mime"
 	"regexp"
+	"strings"
 )
+
+func GetMimeType(objectName string) (bool, string) {
+	mimeType := mime.TypeByExtension(objectName)
+
+	if mimeType == "" {
+		return false, ""
+	}
+
+	return true, mimeType
+}
 
 func ValidateMimeTypes(mimeTypes []string) (bool, string) {
 	mimeTypePattern := regexp.MustCompile(`^[a-zA-Z0-9-]+/[a-zA-Z0-9-+]+$`)
@@ -18,9 +30,29 @@ func ValidateMimeTypes(mimeTypes []string) (bool, string) {
 
 	for _, mimeType := range mimeTypes {
 		if !mimeTypePattern.MatchString(mimeType) {
-			return false, fmt.Sprintf("'%s' is invalid mime type", mimeType)
+			return false, fmt.Sprintf(`'%s' is invalid mime type`, mimeType)
 		}
 	}
 
 	return true, ""
+}
+
+func ValidateAllowedMimeTypes(mimeType string, allowedMimeTypes []string) (bool, string) {
+	if len(allowedMimeTypes) == 0 {
+		return true, ""
+	}
+
+	if len(allowedMimeTypes) == 1 && allowedMimeTypes[0] == "*" {
+		return true, ""
+	}
+
+	mimeType = strings.ToLower(mimeType)
+
+	for _, allowed := range allowedMimeTypes {
+		if mimeType == strings.ToLower(allowed) {
+			return true, ""
+		}
+	}
+
+	return false, fmt.Sprintf(`expected mime types '%s' but got '%s'`, strings.Join(allowedMimeTypes, ","), mimeType)
 }
