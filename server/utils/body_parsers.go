5359bcf588b9f859332d071ad5f9c9822c12e68e
ArkamFahry <92285161+ArkamFahry@users.noreply.github.com>
2023-08-13 20:50:21 +0530

    updated the body parser to be more general

--- server/utils/body_parsers.go
+++ server/utils/body_parsers.go
@@ -5,10 +5,12 @@ import (
 	"github.com/ArkamFahry/uploadnexus/server/errors"
 )
 
-func ParseRequestBody(body []byte, data interface{}) *errors.HttpError {
+func ParseRequestBody(body []byte, data interface{}) error {
+	const Op errors.Op = "utils.ParseRequestBody"
+
 	err := json.Unmarshal(body, data)
 	if err != nil {
-		return errors.NewBadRequestError("invalid request body")
+		return errors.NewError(Op, "invalid request body", err)
 	}
 
 	return nil
